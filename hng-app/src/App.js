72ebccf18acccb33ac1619f128747f18fc4966ac
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-10-27 11:33:18 +0300

    created components

--- hng-app/src/App.js
+++ hng-app/src/App.js
@@ -1,5 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Background from './Backgroung.js';
+import Books from './Books.js';
+import Design from './Design.js';
+import Footer from './Footer.js';
+import Profile from './Profile.js';
+import Python from './Python.js';
+import Team from './Team.js';
+import Twitter from './Twitter.js';
 
 function App() {
   return (
